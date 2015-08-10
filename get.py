70be1d95203b43724277555d3cd0a22dd81a1af8
s0urc3d3v3l0pm3nt <author@techredesign.net>
2015-08-10 15:11:34 -0400

    fixed code styling

--- get.py
+++ get.py
@@ -71,13 +71,13 @@ def download_package(type, package_name):
             break
     URLlines = [line.rstrip('\n') for line in open("packageURL")]
     url = URLlines[index]
-    if (type is 0):
+    if type is 0:
         ftp_download(package_name, url)
-    elif (type is 1):
+    elif type is 1:
         http_download(package_name, url)
-    elif (type is 2):
+    elif type is 2:
         git_download(package_name, url)
-    elif (type is 3):
+    elif type is 3:
         subversion_download(package_name, url)
     else:
         print("ERROR: download function could not be run for an unknown reason")
@@ -86,15 +86,15 @@ def download_package(type, package_name):
 def check_type(package_name):
     FTPList = [line.rstrip('\n') for line in open("FTPindex")]
     for i in FTPList:
-        if (i is package_name):
+        if i is package_name:
             return 0
     HTTPList = [line.rstrip('\n') for line in open("HTTPindex")]
     for iter in HTTPList:
-        if (iter is package_name):
+        if iter is package_name:
             return 1
     GITlist = [line.rstrip('\n') for line in open("GITindex")]
     for iterate in GITlist:
-        if (iterate is package_name):
+        if iterate is package_name:
             return 2
     SUBVERSIONlist = [line.rstrip('\n') for line in open("SUBVERSIONlist")]
     for iteration in SUBVERSIONlist:
@@ -118,13 +118,13 @@ def main():
     package_name = args.package
     if verify_package(package_name):
         type = check_type(package_name)
-        if (type is 0):
+        if type is 0:
             download_package(0, package_name)
-        elif (type is 1):
+        elif type is 1:
             download_package(1, package_name)
-        elif (type is 2):
+        elif type is 2:
             download_package(2, package_name)
-        elif (type is 3):
+        elif type is 3:
             download_package(3, package_name)
         else:
             print("Error: package download method could not be found")
