617c16c96302ab3fd799a49b7824844a72bba1b7
s0urc3 d3v3l0pm3nt <author@techredesigned.com>
2019-02-07 20:56:18 -0500

    remove unrendered \n chars

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Vor diese Anwendung ausfueren koennen, muessen Sie, voraussetzung paket installieren\nDies Anwendung installieren \"wget\" durch apt-get \n Es ist eine moerchlichkeiten mehr werden installiert durch apt, falls es ist boenertigt"
+echo "Vor diese Anwendung ausfueren koennen, muessen Sie, voraussetzung paket installieren. Dies Anwendung installieren \"wget\" durch apt-get. Es ist eine moerchlichkeiten mehr werden installiert durch apt, falls es ist boenertigt"
 echo "Fortfahren? [y,n]"
 read weiter
 if [[ "$weiter" == "y" ]]; then
